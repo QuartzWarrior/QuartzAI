e9c1f9bcdb22967c6209009b1f1b7f9cf6b9c9c2
xerous-dev <168219048+xerous-dev@users.noreply.github.com>
2024-10-04 19:05:13 +0100

    Added More Models To /ask-ai

--- bot/cogs/AI/ask-ai.py
+++ bot/cogs/AI/ask-ai.py
@@ -8,7 +8,10 @@ class Ask_ai(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @nextcord.slash_command(name="ask-ai", description="Ask AI A Question")
+    @nextcord.slash_command(
+            name="ask-ai",
+            description="Ask AI A Question"
+            )
     async def ask_ai(
         self,
         ctx: nextcord.Interaction,
@@ -16,12 +19,13 @@ class Ask_ai(commands.Cog):
         model: str = SlashOption(
             name="model",
             description="Select The Model To Use",
-            choices={"GPT-4o": "gpt-4o"}),
+            choices={"GPT 4o": "gpt-4o", "GPT 4o Mini":"gpt-4o-mini", "GPT 4":"gpt-4", "GPT 4 Turbo":"gpt-4-turbo", "Gemini Pro":"gemini-pro", "Claude 3 Haiku":"claude-3-haiku", "Claude 3 Sonnet":"claude-3-sonnet"}),
     ):
         e1 = nextcord.Embed(
-                               title="AI - Ask AI",
-                               description="Please Wait...",
-                               color=nextcord.Color.purple())
+                title="AI - Ask AI",
+                description="Please Wait...",
+                color=nextcord.Color.purple()
+                )
 
         msg  = await ctx.send(embed=e1)
 
@@ -32,10 +36,10 @@ class Ask_ai(commands.Cog):
         messages=[{"role": "user", "content": f"{prompt}"}],
     )
         e2 = nextcord.Embed(
-                               title="AI - Ask AI",
-                               description=f"> **Model:** {model}\n> **Prompt:**  {prompt}\n> **Response:** {response.choices[0].message.content}",
-                               color=nextcord.Color.purple()
-                               )
+                title="AI - Ask AI",
+                description=f"> **Model:** {model}\n> **Prompt:**  {prompt}\n> **Response:** {response.choices[0].message.content}",
+                color=nextcord.Color.purple()
+                )
         await msg.edit(embed=e2)
 
         
