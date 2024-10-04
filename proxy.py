7a51fa98d0d9f96b7e5237afd0f5878f4da23c2f
xerous-dev <168219048+xerous-dev@users.noreply.github.com>
2024-10-04 01:16:29 +0100

    Deleted Old Code ( Making Way For V2 )

--- proxy.py
+++
@@ -1,8 +0,0 @@
-import random
-import ujson
-
-user_pass = ujson.load(open("config.json"))["proxy_username_password"]
-
-proxy = random.choice(open("proxies.csv").read().splitlines()).split(",")
-
-print("https://" + user_pass + "@" + proxy[1] + ":" + proxy[2])
